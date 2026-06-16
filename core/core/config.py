6e72d0dac835843a16daee8c016c1781177d2bc8
aliganji14 <aliganji142879@gmail.com>
2026-06-16 13:35:58 +0330

    Send email in Fastapi and test with smtp4dev

--- core/core/config.py
+++ core/core/config.py
@@ -7,5 +7,17 @@ class Settings(BaseSettings):
     REDIS_URL: str = "redis://localhost:6379"
     model_config = SettingsConfigDict(env_file=".env")
 
+    MAIL_USERNAME: str = ""
+    MAIL_PASSWORD: str = ""
+    MAIL_FROM: str = "no-reply@example.com"
+    MAIL_PORT: int = 25
+    MAIL_SERVER: str = "smtp4dev"
+    MAIL_FROM_NAME: str = "Admin"
+    MAIL_STARTTLS: bool = False
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = False
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
