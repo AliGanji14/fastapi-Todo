862df64322a6d0f2460c4a5561752facb41aafc6
aliganji14 <aliganji142879@gmail.com>
2026-06-18 12:32:29 +0330

    How to request with an authenticated user

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -2,6 +2,20 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+
+    def on_start(self):
+        response = self.client.post("/users/login", json={
+            "username": "aliganji", 
+            "password": "12345",
+        })
+        access_token = response.json()['access_token']
+        self.client.headers = {"Authorization": f'Bearer {access_token}'}
+
+    @task
+    def tasks_list(self):
+        self.client.get("/tasks")
+
     @task
     def tasks_list(self):
         self.client.get("/tasks")
