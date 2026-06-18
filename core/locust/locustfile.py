6642892af6420ae3586523cd8640f6b19dc6f71c
aliganji14 <aliganji142879@gmail.com>
2026-06-18 12:17:20 +0330

    Creating tasks for testing

--- core/locust/locustfile.py
+++ core/locust/locustfile.py
@@ -1,4 +1,19 @@
-from locust import HttpUser,task,between
+from locust import HttpUser, task, between
+
 
 class QuickstartUser(HttpUser):
-    pass
+    @task
+    def tasks_list(self):
+        self.client.get("/tasks")
+
+    @task
+    def fetch_weather(self):
+        self.client.get("/fetch-current-weather")
+
+    @task
+    def initiate_task(self):
+        self.client.get("/initiate-task")
+
+    @task
+    def not_found(self):
+        self.client.get("/not-found")
